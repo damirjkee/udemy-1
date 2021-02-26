20fbf0eccdea0481f55d33209d032ebc5379a2ef
Damir <damirka.mukanov@gmail.com>
2021-02-26 20:55:40 +0600

    создал 1 часть приложения

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -6,4 +6,15 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false
-}
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', '');
+const b = +prompt('На сколько оцените его?', '');
+
+const c = prompt('Один из последних просмотренных фильмов?', '');
+const d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
