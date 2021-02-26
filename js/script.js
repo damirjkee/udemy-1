848533846ab44e8d6fb3825fec460d417fb62eb8
Damir <damirka.mukanov@gmail.com>
2021-02-26 20:31:59 +0600

    Создал вопрос пользователю и создал объект

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
