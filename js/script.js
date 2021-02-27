f772e64fac02fcca783ef672a69a0045f8bc2e15
Damir <damirka.mukanov@gmail.com>
2021-02-28 02:18:01 +0600

    2 часть практики

--- js/script.js
+++ js/script.js
@@ -8,13 +8,26 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '');
-const b = +prompt('На сколько оцените его?', '');
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
-const c = prompt('Один из последних просмотренных фильмов?', '');
-const d = +prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = +prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
