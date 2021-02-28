35a06f8ef34e67606dbbd7ff67bebe10bae07d45
Damir <damirka.mukanov@gmail.com>
2021-03-01 01:48:54 +0600

    Убрал комментарий

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function rememberMyFilms() {
     }
 }
 
-// rememberMyFilms();
+rememberMyFilms();
 
 function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
@@ -63,3 +63,4 @@ function writeYourGenres() {
 }
 
 writeYourGenres();
+
